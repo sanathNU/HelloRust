519f5d393d3af924be93b05ab7011437b7c04d1b
Sanath <sanathn.upadhyaya@gmail.com>
2024-08-23 19:33:32 +0530

    some functions of level 9

--- src/level_9.rs
+++ src/level_9.rs
@@ -1,1 +1,39 @@
 use text_io::read;
+use std::io;
+use std::collections::HashMap;
+
+// **Case Reference Conversion**
+pub fn convert_case() {
+    // Get user input
+    println!("Enter the lowercase string to be case converted:");
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let mut input:String = input;
+
+    // Print the original string
+    println!("The String before case conversion is: {}", input);
+
+    convert_char_case(&mut input);
+    println!("The String after case conversion is: {}", input);
+}
+
+fn convert_char_case(input: &mut String) {
+    let uppercased: String = input.chars().map(|c| c.to_uppercase()).flatten().collect();
+    *input = uppercased;
+}
+
+// **Book Details
+// The book details are pulled from a input file, hence even completing the function. There will also be a function to calculate file statistics
+struct Library {
+    books: HashMap<String,Vec<Book>>
+}
+struct Book {
+    name: String,
+    author: String,
+    pages: i32
+}
+
+fn file_statistics(){}
+fn read_input(){} //reads input from the file
+fn create_library(){} //reads values from the file to create a genre of books
+fn write_library(){} //writes the entire library to a file
--- src/main.rs
+++ src/main.rs
@@ -18,5 +18,5 @@ fn main() {
     // level_1::favapps();
     // level_1::stud_read_write();
     // level_2::vowel_or_consonant();
-    level_8::array_rotation()
+    level_9::convert_case();
 }
